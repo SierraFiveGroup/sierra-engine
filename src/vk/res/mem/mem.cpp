f631d8aa681c0fb025f0bc8b62783feedb2ce232
kweezo <jakobtadejv@gmail.com>
2025-09-28 11:01:15 +0200

    [UNTESTED] Added basic vulkan memory allocation with VMA

---
+++ src/vk/res/mem/mem.cpp
@@ -0,0 +1,123 @@
+#include "mem.hpp"
+
+#define VMA_IMPLEMENTATION /*sum bullshit*/
+#define VMA_sTATIC_VULKAN_FUNCTIONS 1 // if yall dont have this god help you
+#include "vk/vk_mem_alloc.h"
+
+namespace Sierra::vlk {
+    static std::unordered_map<VkDevice, std::shared_ptr<VmaAllocator>> allocators; 
+    ResourceRef<VmaAllocator> getAllocator(Context& context) {
+        auto it = allocators.find(context.device->getDevice());
+        if (it == allocators.end()) {
+            throw new std::runtime_error("Tried to allocate memory from nonexistent allocator");
+        }
+
+        return it->second;
+    }
+
+
+    uint32_t Mem::ALLOCATION_CREATE_DEDICATED_MEMORY_BIT = VMA_ALLOCATION_CREATE_DEDICATED_MEMORY_BIT;
+    uint32_t Mem::ALLOCATION_CREATE_NEVER_ALLOCATE_BIT = VMA_ALLOCATION_CREATE_NEVER_ALLOCATE_BIT;
+    uint32_t Mem::ALLOCATION_CREATE_MAPPED_BIT = VMA_ALLOCATION_CREATE_MAPPED_BIT;
+    uint32_t Mem::ALLOCATION_CREATE_WITHIN_BUDGET_BIT = VMA_ALLOCATION_CREATE_WITHIN_BUDGET_BIT;
+    uint32_t Mem::ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT = VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT;
+    uint32_t Mem::ALLOCATION_CREATE_HOST_ACCESS_RANDOM_BIT = VMA_ALLOCATION_CREATE_HOST_ACCESS_RANDOM_BIT;
+    uint32_t Mem::ALLOCATION_CREATE_HOST_ACCESS_ALLOW_TRANSFER_INSTEAD_BIT = VMA_ALLOCATION_CREATE_HOST_ACCESS_ALLOW_TRANSFER_INSTEAD_BIT;
+    uint32_t Mem::ALLOCATION_CREATE_STRATEGY_MIN_MEMORY_BIT = VMA_ALLOCATION_CREATE_STRATEGY_MIN_MEMORY_BIT;
+    uint32_t Mem::ALLOCATION_CREATE_STRATEGY_MIN_TIME_BIT = VMA_ALLOCATION_CREATE_STRATEGY_MIN_TIME_BIT;
+
+    void Mem::init(Context& context) {
+        if (allocators.find(context.device->getDevice()) != allocators.end()) {
+            WARN("Trying to create a preexistent VmaAllocator with device " << context.device->getDevice());
+            return;
+        }
+
+        VmaAllocator allocator = (VmaAllocator)createAllocator(context);
+        allocators[context.device->getDevice()] = std::make_shared<VmaAllocator>(allocator);
+    }
+
+    void Mem::destroy(Context& context) {
+        auto it = allocators.find(context.device->getDevice());
+
+        if (it != allocators.end()) {
+            WARN("Trying to destroy a nonexistent VmaAllocator with device " << context.device->getDevice());
+            return;
+        }
+
+        vmaDestroyAllocator(*it->second);
+        allocators.erase(it);
+    }
+
+    VmaAllocator Mem::createAllocator(Context& context) { //TODO fetch the fucking extensions
+        VmaAllocatorCreateInfo allocInfo{};
+        //allocInfo.flags = ...
+        allocInfo.vulkanApiVersion = VK_API_VERSION_1_3;
+        allocInfo.physicalDevice = context.device->getPhysicalDevice();
+        allocInfo.device = context.device->getDevice();
+        allocInfo.instance = context.instance->getInstance();
+
+        VmaAllocator allocator;
+        VK_ERR(vmaCreateAllocator(&allocInfo, &allocator));
+
+        return allocator;
+    }
+
+
+    Mem::Mem(): allocator(), allocation(), buffer(), image() {
+        
+    }
+
+    Mem::Mem(Context& context, Info& info, VkBufferCreateInfo& bufferInfo, VkBuffer& buffer): allocator(), buffer(), image() {
+        allocator = getAllocator(context);
+        createBuffer(info, bufferInfo, buffer);
+
+        this->buffer = buffer;
+    }
+
+    Mem::Mem(Context& context, Info& info, VkImageCreateInfo& imageInfo, VkImage& image): buffer(), image() {
+        allocator = getAllocator(context);
+        createImage(info, imageInfo, image);
+
+        this->image = image;
+    }
+
+    void Mem::createBuffer(Info& info, VkBufferCreateInfo& bufferInfo, VkBuffer& buffer) {
+        VmaAllocationCreateInfo allocInfo{};
+        allocInfo.flags = info.flags;
+        allocInfo.requiredFlags = info.required;
+        allocInfo.preferredFlags = info.preferred;
+        allocInfo.priority = info.priority;
+        allocInfo.usage = (VmaMemoryUsage)info.usage;
+
+        VK_ERR(vmaCreateBuffer(*allocator.getLock(), &bufferInfo, &allocInfo, &buffer, &allocation, nullptr));
+    }
+
+    void Mem::createImage(Info& info, VkImageCreateInfo& imageInfo, VkImage& image) {
+        VmaAllocationCreateInfo allocInfo{};
+        allocInfo.flags = info.flags;
+        allocInfo.requiredFlags = info.required;
+        allocInfo.preferredFlags = info.preferred;
+        allocInfo.priority = info.priority;
+        allocInfo.usage = (VmaMemoryUsage)info.usage;
+
+        VK_ERR(vmaCreateImage(*allocator.getLock(), &imageInfo, &allocInfo, &image, &allocation, nullptr));
+    }
+
+    Mem::~Mem() {
+        if (buffer)
+            vmaDestroyBuffer(*allocator.getLock(), buffer, allocation);
+        else if(image)
+            vmaDestroyImage(*allocator.getLock(), image, allocation);
+    }
+
+    ///////
+
+    MemPool::MemPool() {
+
+    }
+
+    MemPool::MemPool(Context& context, Info& info): allocator() {
+        allocator = getAllocator(context);
+    }
+
+}
---
+++ src/vk/res/mem/mem.hpp
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "vk/core/context.hpp"
+#include "vk/util.hpp"
+#include "scene/ref/resource_ref.hpp"
+
+#include "vk/vk_mem_alloc.h"
+
+#include <unordered_map>
+#include <memory>
+
+
+namespace Sierra::vlk {
+    class Mem {
+        friend class MemPool;
+
+        public:
+            static uint32_t ALLOCATION_CREATE_DEDICATED_MEMORY_BIT;
+            static uint32_t ALLOCATION_CREATE_NEVER_ALLOCATE_BIT;
+            static uint32_t ALLOCATION_CREATE_MAPPED_BIT;
+            static uint32_t ALLOCATION_CREATE_WITHIN_BUDGET_BIT;
+            static uint32_t ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT;
+            static uint32_t ALLOCATION_CREATE_HOST_ACCESS_RANDOM_BIT;
+            static uint32_t ALLOCATION_CREATE_HOST_ACCESS_ALLOW_TRANSFER_INSTEAD_BIT;
+            static uint32_t ALLOCATION_CREATE_STRATEGY_MIN_MEMORY_BIT;
+            static uint32_t ALLOCATION_CREATE_STRATEGY_MIN_TIME_BIT;
+
+            enum class Usage {
+                AUTO,
+                PREFER_DEVICE,
+                PREFER_HOST,
+            };
+
+            struct Info {
+                size_t flags;
+                Usage usage;
+                VkMemoryPropertyFlags required;
+                VkMemoryPropertyFlags preferred;
+                float priority;
+            };
+
+            /// STATIC PART
+            static void init(Context& context);
+            static void destroy(Context& context);
+            /////////
+
+            Mem();
+            Mem(Context& context, Info& info, VkBufferCreateInfo& bufferInfo, VkBuffer& buffer);
+            Mem(Context& context, Info& info, VkImageCreateInfo& imageInfo, VkImage& image);
+
+            Mem(Mem&) = delete;
+            Mem(Mem&&) = delete;
+
+            ~Mem();
+
+            /// STATIC PART
+        protected:
+        private:
+            static VmaAllocator createAllocator(Context& context);
+            /////////
+
+            void createBuffer(Info& info, VkBufferCreateInfo& bufferInfo, VkBuffer& buffer);
+            void createImage(Info& info, VkImageCreateInfo& imageInfo, VkImage& image);
+
+            VkBuffer buffer;
+            VkImage image;
+
+            VmaAllocation allocation;
+            ResourceRef<VmaAllocator> allocator;
+    };
+
+    class MemPool {
+        //TODO this is simply in place if ever needed, for now I'll just use the pools that are alloc'd by default 
+        // 2 in 1 file?? this shit is absolutely fucking me over
+        //type of design that makes me want to saw my head clean off
+        //yes I will bitch about it endlessly
+        public:
+            struct Info {
+                uint32_t memTypeIndex;
+                size_t minBlockCount;
+                size_t maxBlockCount;
+                float priority;
+                VkDeviceSize minAlignment;
+            };
+
+            MemPool();
+            MemPool(Context& context, Info& info);
+        private:
+
+            ResourceRef<VmaAllocator> allocator;
+    };
+}
