79952b3ef21c1ebbd31795db6541719881dfa810
Jakob Tadej Vrtacnik <jakobtadejv@gmail.com>
2025-10-03 21:33:36 +0200

    [ADD][UNTESTED] Added a manager for uploading stuff to vulkan resource

---
+++ src/vk/res/mem/mem_manager.cpp
@@ -0,0 +1,104 @@
+#include "mem_manager.hpp"
+
+namespace Sierra::vlk {
+    MemoryManager::MemoryManager(Context& context): asyncDat(std::make_shared<AsyncDat>()),
+     task(Task::Stage::UPLOAD, &MemoryManager::asyncTransfer, std::reinterpret_pointer_cast<uint8_t>(asyncDat)) {
+        asyncDatMutex = std::make_shared<std::mutex>();
+
+        cmdPool = CommandPool(context, context.device->getQueueFamilyIndex(VK_QUEUE_TRANSFER_BIT), VK_COMMAND_POOL_CREATE_TRANSIENT_BIT);
+        cmdBuf = CommandBuffer(cmdPool, VK_COMMAND_BUFFER_LEVEL_PRIMARY);
+
+        asyncDat->cmdBuf = &cmdBuf;
+        asyncDat->context = &context;
+        asyncDat->mutex = asyncDatMutex;
+    }
+
+    void MemoryManager::addTransferOp(TransferOp op) {
+        std::lock_guard lock(*asyncDatMutex);
+        transferOps.push_back(op);
+    }
+
+    void MemoryManager::asyncTransfer(std::shared_ptr<uint8_t> asyncDat, std::function<void()> finish) {
+        AsyncDat& dat = *(AsyncDat*)asyncDat.get();
+
+        dat.cmdBuf->begin(nullptr);
+
+        
+        dat.mutex->lock();
+        for (TransferOp& op : *dat.transferOps) {
+            if(op.buffer) { // copy to buff
+                VkBufferCopy copyRegion{};
+                copyRegion.srcOffset = op.srcOffset;
+                copyRegion.dstOffset = op.dstOffset;
+                copyRegion.size = op.size;
+
+                vkCmdCopyBuffer(dat.cmdBuf->getCommandBuffer(), op.src, op.buffer, 1, &copyRegion);
+                continue;
+            }
+            //copy to image   
+
+            VkBufferImageCopy copyRegion{};
+            copyRegion.bufferOffset = op.srcOffset;
+            copyRegion.bufferRowLength = 0;
+            copyRegion.bufferImageHeight = 0;
+            copyRegion.imageSubresource = op.subResLayers;
+            copyRegion.imageOffset = {0};
+            copyRegion.imageExtent = op.imageExtent;
+
+            vkCmdCopyBufferToImage(dat.cmdBuf->getCommandBuffer(), op.src, op.image, op.imageLayout, 1, &copyRegion);
+        }
+        dat.mutex->unlock();
+
+        dat.cmdBuf->end();
+
+        VkCommandBuffer cmdBufHandle = dat.cmdBuf->getCommandBuffer();
+
+        Fence fence = Fence(*dat.context);
+
+        VkSubmitInfo submitInfo{};
+        submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+        submitInfo.waitSemaphoreCount = 0;
+        submitInfo.signalSemaphoreCount = 0;
+
+        submitInfo.commandBufferCount = 1;
+        submitInfo.pCommandBuffers = &cmdBufHandle;
+
+        VK_ERR(vkQueueSubmit(dat.context->device->getQueue(VK_QUEUE_TRANSFER_BIT), 1, &submitInfo, fence.getFence()));
+
+        VkFence fenceHandle = fence.getFence();
+
+        vkWaitForFences(dat.context->device->getDevice(), 1, &fenceHandle, VK_TRUE, (uint64_t)-1);
+    }
+
+    MemoryManager::MemoryManager(MemoryManager&& other) {
+        std::lock_guard lock(*asyncDatMutex);
+
+        transferOps = std::move(other.transferOps);
+        cmdPool = std::move(other.cmdPool);
+        cmdBuf = std::move(other.cmdBuf);
+        task = other.task;
+        asyncDat = other.asyncDat;
+        asyncDatMutex = other.asyncDatMutex;
+
+        asyncDat->cmdBuf = &cmdBuf;
+
+        other.task = Task();
+        other.asyncDat.reset();
+    }
+
+    void MemoryManager::operator=(MemoryManager&& other) {
+        std::lock_guard lock(*asyncDatMutex);
+
+        transferOps = std::move(other.transferOps);
+        cmdPool = std::move(other.cmdPool);
+        cmdBuf = std::move(other.cmdBuf);
+        task = other.task;
+        asyncDat = other.asyncDat;
+        asyncDatMutex = other.asyncDatMutex;
+
+        asyncDat->cmdBuf = &cmdBuf;
+
+        other.task = Task();
+        other.asyncDat.reset();
+    }
+}
--- src/vk/res/mem/mem_manager.hpp
+++ src/vk/res/mem/mem_manager.hpp
@@ -1,25 +1,66 @@
 #pragma once
 
+#include <vector>
+#include <mutex>
+
 #include <vulkan/vulkan.hpp>
+
+#include "vk/res/command_buffer/command_buffer.hpp"
+#include "vk/core/context.hpp"
+#include "vk/sync/fence/fence.hpp"
+
+#include "scene/tasks/task.hpp"
 
 namespace Sierra::vlk {
 
     class MemoryManager {
         public:
             struct TransferOp {
-                void* src;
+                VkBuffer src;
 
                 size_t size;
-                size_t offset;
+
+                size_t srcOffset;
+                size_t dstOffset;
+
+                VkImageSubresourceLayers subResLayers;
+                VkExtent3D imageExtent;
+                VkImageLayout imageLayout;
             
                 VkBuffer buffer;
                 VkImage image;
             };
 
-            MemoryManager();
+            MemoryManager(Context& context);
 
             MemoryManager(MemoryManager&) = delete;
-            MemoryManager(MemoryManager&&) = delete;
+
+            MemoryManager(MemoryManager&&); 
+            void operator=(MemoryManager&&);
+
+            void addTransferOp(TransferOp op);
+
+        protected:
+            Task getTask();
         private:
+            struct AsyncDat {
+                Context* context;
+
+                std::shared_ptr<std::mutex> mutex;
+
+                std::vector<TransferOp>* transferOps;
+                CommandBuffer* cmdBuf;
+            };
+
+            static void asyncTransfer(std::shared_ptr<uint8_t> asyncDat, std::function<void()> finish);
+
+            std::vector<TransferOp> transferOps;
+
+            CommandPool cmdPool;
+            CommandBuffer cmdBuf;
+
+            Task task;
+            std::shared_ptr<AsyncDat> asyncDat;
+            std::shared_ptr<std::mutex> asyncDatMutex;
     };
 }
