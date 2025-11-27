763b56a3c55df5bd88ac3ca12b67756a5b2b834b
kweezo <jakobtadejv@gmail.com>
2025-11-27 21:41:06 +0100

    [UPD] Fixed and improved VK_ERR macro

--- src/vk/util.hpp
+++ src/vk/util.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <stdexcept>
+#include <string>
 
 #include "vulkan/vulkan.h"
 
 #define STRINGIFY(x) #x
-#define VK_ERR(x) if(x != VK_SUCCESS) {throw std::runtime_error("Error check " #x " on " __FILE__ ":" STRINGIFY(__LINE__) " failed with code " STRINGIFY(x));}
+#define STRINGIFY_HELP(x) STRINGIFY(x)
+#define VK_ERR(x) {VkResult ret{}; if((ret = x) != VK_SUCCESS) {throw std::runtime_error("Error check " #x " on " __FILE__ ":" STRINGIFY_HELP(__LINE__) " failed with code " + std::to_string(ret));}}
 #define VK_ASSERT(x) if (!(x)) {throw std::runtime_error("Assert " #x " failed");}
