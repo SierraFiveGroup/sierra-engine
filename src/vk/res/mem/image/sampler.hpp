47a675717ad0484e06e2f1dec084c49b6a0bdea8
kweezo <jakobtadejv@gmail.com>
2025-12-07 20:25:56 +0100

    [ADD] Image sampler

---
+++ src/vk/res/mem/image/sampler.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+
+#include "vk/core/context.hpp"
+
+namespace Sierra::vlk {
+    class Sampler {
+        public:
+            struct Info {
+                VkFilter filter; 
+                VkSamplerMipmapMode mipmapMode;
+                VkSamplerAddressMode addressMode;
+                float lodBias;
+                float minLod;
+                float maxLod;
+                bool anisotropy;
+                float maxAnisotropy;
+            };
+
+            Sampler();
+            Sampler(Context& context, Info info);
+
+            Sampler(Sampler&) = delete;
+            Sampler operator=(Sampler&) = delete;
+
+            Sampler(Sampler&&) = default;
+            void operator=(Sampler&&);
+
+            ~Sampler();
+
+            VkSampler getSampler();
+        private:
+            void createSampler(Info& info);
+
+            VkSampler sampler;
+            Context* context;
+    };
+}
