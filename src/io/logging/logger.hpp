3f0426142ce8b7bfa159318a8b2bfdbf06ebd6c6
DcruBro <jonaskn914@gmail.com>
2025-12-07 15:14:32 +0100

    [FIX] Make CMake configurable on Windows, build still fails due to std lib issues.

--- src/io/logging/logger.hpp
+++ src/io/logging/logger.hpp
@@ -4,18 +4,35 @@
 #include <cstdio>
 
 #ifndef LOG
+
+#ifndef WIN32 || _WIN32
 #define LOG(msg) \
     std::cout << "\033[0m(LOG) " << __PRETTY_FUNCTION__ << ": " << msg << "\n"
+#else
+#define LOG(msg) \
+    std::cout << "\033[0m(LOG) " << ": " << msg << "\n"
+#endif
+
 #endif
 
 #ifndef WARN
+#ifndef WIN32 || _WIN32
 #define WARN(msg) \
     std::cerr << "\033[33m(WARN) \033[0m" << __PRETTY_FUNCTION__ << ": " << msg << "\n"
+#else
+#define WARN(msg) \
+    std::cerr << "\033[33m(WARN) \033[0m" << ": " << msg << "\n"
+#endif
 #endif
 
 #ifndef ERROR
+#ifndef WIN32 || _WIN32
 #define ERROR(msg) \
     std::cerr << "\033[31m(ERROR) \033[0m" << __PRETTY_FUNCTION__ << ": " << msg << "\n"
+#else
+#define ERROR(msg) \
+    std::cerr << "\033[31m(ERROR) \033[0m" << ": " << msg << "\n"
+#endif
 #endif
 
 #ifndef DBG
