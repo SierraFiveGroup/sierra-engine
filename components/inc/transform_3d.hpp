63e9fffcbcb34c96294e41803a5bfb9aebe310f6
kweezo <jakobtadejv@gmail.com>
2026-01-12 20:29:43 +0100

    [ADD] Added Transform3D

---
+++ components/inc/transform_3d.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+
+#include "scene/component/component.hpp"
+#include "logging/logger.hpp"
+
+#include <glm/glm.hpp>
+
+SIERRA_COMP(Transform3D)
+    public:
+        Transform3D();
+
+        void update() override;
+
+        void setPos(glm::vec3 vec);
+        void setRotation(glm::vec3 vec);
+        void setScale(glm::vec3 vec);
+    private: //the ONLY 3 members allowed to be here
+        glm::mediump_vec3 pos;
+        glm::mediump_vec3 rotation;
+        glm::mediump_vec3 scale;
+};
---
+++ components/src/transform_3d.cpp
@@ -0,0 +1,20 @@
+#include "transform_3d.hpp"
+
+SIERRA_GEN_COMP(Transform3D);
+
+
+void Transform3D::update() {
+
+}
+
+void Transform3D::setPos(glm::vec3 vec) {
+    pos = vec;
+}
+
+void Transform3D::setRotation(glm::vec3 vec) {
+    rotation = vec;
+}
+
+void Transform3D::setScale(glm::vec3 vec) {
+    scale = vec; 
+}
